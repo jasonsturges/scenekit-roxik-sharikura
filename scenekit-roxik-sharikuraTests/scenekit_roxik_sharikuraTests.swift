22c9eda7920651087663c99f63f9b1d7488fe700
Jason Sturges <jason@jasonsturges.com>
2020-12-09 01:54:06 -0600

    Initial Commit

---
+++ scenekit-roxik-sharikuraTests/scenekit_roxik_sharikuraTests.swift
@@ -0,0 +1,33 @@
+//
+//  scenekit_roxik_sharikuraTests.swift
+//  scenekit-roxik-sharikuraTests
+//
+//  Created by Jason Sturges on 12/9/20.
+//
+
+import XCTest
+@testable import scenekit_roxik_sharikura
+
+class scenekit_roxik_sharikuraTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
